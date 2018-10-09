bf17840171539f74f7019394a13b3d4a19fee37d
Rui <2221870259@qq.com>
2018-10-10 01:06:34 +0800

    update

--- source/msw_input_device.h
+++ source/msw_input_device.h
@@ -16,30 +16,14 @@ namespace vinyl
 			virtual void setCaptureObject(WindHandle window) noexcept override;
 			virtual WindHandle getCaptureObject() const noexcept override;
 
-			virtual bool peekEvents(InputEvent& event) noexcept override;
-			virtual bool pollEvents(InputEvent& event) noexcept override;
-			virtual bool waitEvents(InputEvent& event) noexcept override;
-			virtual bool waitEvents(InputEvent& event, int timeout) noexcept override;
-			virtual void flushEvent() noexcept override;
-
 			virtual IInputDevicePtr clone() const noexcept override;
-
-		private:
-			void update() noexcept;
 
 		private:
 			MSWInputDevice(const MSWInputDevice&) noexcept = delete;
 			MSWInputDevice& operator=(const MSWInputDevice&) noexcept = delete;
 
 		private:
-			HWND window_;
-
-			bool isButtonPress_;
-
-			int button_;
-
-			int mouseX_;
-			int mouseY_;
+			WindHandle window_;
 		};
 	}
 }

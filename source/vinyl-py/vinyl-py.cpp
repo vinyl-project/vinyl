629e9208fe7058d0dde68778f6e0fd7d994f4f3d
WangJun <1103870790@qq.com>
2018-10-13 23:47:51 +0800

    bind get_mouse_pos and get_desktop_size

--- source/vinyl-py/vinyl-py.cpp
+++ source/vinyl-py/vinyl-py.cpp
@@ -31,6 +31,9 @@ PyObject* vinyl_is_key_up(PyObject* self, PyObject* args);
 PyObject* vinyl_is_mouse_button_down(PyObject* self, PyObject* args);
 PyObject* vinyl_is_mouse_button_up(PyObject* self, PyObject* args);
 
+PyObject* vinyl_get_mouse_pos(PyObject* self, PyObject* args);
+PyObject* vinyl_get_desktop_size(PyObject* self, PyObject* args);
+
 PyObject* vinyl_sleep(PyObject* self, PyObject* args);
 PyObject* vinyl_message_box(PyObject* self, PyObject* args);
 PyObject* vinyl_trace_print(PyObject* self, PyObject* args);
@@ -74,6 +77,10 @@ static PyMethodDef VinylMethods[] = {
 	"A function that controls mouse to relative move." },
 	{ "mouse_wheel", vinyl_mouse_wheel, METH_VARARGS,
 	"A function that controls mouse wheel." },
+	{ "get_desktop_size", vinyl_get_desktop_size, METH_VARARGS,
+	"A function that gets size of desktop." },
+	{ "get_mouse_pos", vinyl_get_mouse_pos, METH_VARARGS,
+	"A function that gets position of mouse." },
 	{ "is_key_down", vinyl_is_key_down, METH_VARARGS,
 	"A function that gets key state." },
 	{ "is_key_up", vinyl_is_key_up, METH_VARARGS,
@@ -499,6 +506,50 @@ PyObject* vinyl_key_click(PyObject* self, PyObject* args)
 	}
 
 	Py_RETURN_NONE;
+}
+
+PyObject* vinyl_get_mouse_pos(PyObject* self, PyObject* args)
+{
+	std::uint16_t x, y;
+	PyObject *pos_tuple = nullptr;
+	try
+	{
+		VinylGetMousePos(x, y);
+		pos_tuple = PyTuple_New(2);
+		if (pos_tuple)
+		{
+			PyTuple_SET_ITEM(pos_tuple, 0, PyLong_FromLong(x));
+			PyTuple_SET_ITEM(pos_tuple, 1, PyLong_FromLong(y));
+		}
+	}
+	catch (const std::exception& e)
+	{
+		PyErr_SetString(StdErrorObj, e.what());
+	}
+
+	return pos_tuple;
+}
+
+PyObject* vinyl_get_desktop_size(PyObject* self, PyObject* args)
+{
+	std::uint16_t w, h;
+	PyObject *size_tuple = nullptr;
+	try
+	{
+		VinylGetDesktopSize(w, h);
+		size_tuple = PyTuple_New(2);
+		if (size_tuple)
+		{
+			PyTuple_SET_ITEM(size_tuple, 0, PyLong_FromLong(w));
+			PyTuple_SET_ITEM(size_tuple, 1, PyLong_FromLong(h));
+		}
+	}
+	catch (const std::exception& e)
+	{
+		PyErr_SetString(StdErrorObj, e.what());
+	}
+
+	return size_tuple;
 }
 
 PyObject* vinyl_is_key_down(PyObject* self, PyObject* args)
